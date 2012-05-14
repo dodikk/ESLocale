60cfc20d6c158b4f924e5c11686097aae6357208
dodikk <dodikk88.reg@gmail.com>
2012-05-14 15:10:57 +0300

    Added test for invalid input

--- ESLocaleTests/WeekCalendarQueryTest.m
+++ ESLocaleTests/WeekCalendarQueryTest.m
@@ -114,7 +114,6 @@
     sqlite3_finalize( statement_ );
 }
 
-
 -(void)testNullArgsResultInError
 {
     {
@@ -177,6 +176,163 @@
         
         sqlite3_finalize( statement_ );
     }
+}
+
+-(void)testArgsCountMismatchIsCheckedBySqlite
+{
+    {
+        int qResult_ = 0;
+        sqlite3_stmt* statement_ = NULL;
+        
+        
+        const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'YYYY-WW', '2011-01-02' )\n"
+        "FROM 'Campaigns'\n"
+        "LIMIT 1;";
+        
+        
+        
+        qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+        STAssertTrue( qResult_ == SQLITE_ERROR, @"unexpected sqlite3_prepare failure" );    
+
+        sqlite3_finalize( statement_ );
+    }
+
+
+
+    {
+        int qResult_ = 0;
+        sqlite3_stmt* statement_ = NULL;
+        
+        
+        const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'YYYY-WW', '2011-01-02', 'en_US', '1', '2', '3' )\n"
+        "FROM 'Campaigns'\n"
+        "LIMIT 1;";
+        
+        
+        
+        qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+        STAssertTrue( qResult_ == SQLITE_ERROR, @"unexpected sqlite3_prepare failure" );    
+        
+        sqlite3_finalize( statement_ );
+    }
+}
+
+-(void)testInvalidDataResultsInNull
+{   
+    {
+        int qResult_ = 0;
+        sqlite3_stmt* statement_ = NULL;
+        const unsigned char* result_ = NULL;
+        
+        
+        const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'blablabla', '2011-01-02', 'en_US' )\n"
+        "FROM 'Campaigns'\n"
+        "LIMIT 1;";
+        
+        
+        
+        qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+        STAssertTrue( qResult_ == SQLITE_OK, @"unexpected sqlite3_prepare failure" );    
+
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( qResult_ == SQLITE_ROW, @"query must have only one record - %d", qResult_ );    
+        STAssertTrue( 1 == sqlite3_column_count( statement_ ), @"column count mismatch" );
+        
+        result_ = sqlite3_column_text( statement_, 0 );
+        STAssertTrue( NULL == result_ , @"raw answer mismatch" );
+        
+        
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( SQLITE_DONE == qResult_, @"query must have only one record" );
+        sqlite3_finalize( statement_ );
+    }
+    
+    
+    {
+        int qResult_ = 0;
+        sqlite3_stmt* statement_ = NULL;
+        const unsigned char* result_ = NULL;
+        
+        
+        const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'YYYY-ww', 'date should have been here', 'en_US' )\n"
+        "FROM 'Campaigns'\n"
+        "LIMIT 1;";
+        
+        
+        
+        qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+        STAssertTrue( qResult_ == SQLITE_OK, @"unexpected sqlite3_prepare failure" );    
+        
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( qResult_ == SQLITE_ROW, @"query must have only one record - %d", qResult_ );    
+        STAssertTrue( 1 == sqlite3_column_count( statement_ ), @"column count mismatch" );
+        
+        result_ = sqlite3_column_text( statement_, 0 );
+        STAssertTrue( NULL == result_ , @"raw answer mismatch" );
+        
+        
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( SQLITE_DONE == qResult_, @"query must have only one record" );
+        sqlite3_finalize( statement_ );
+    }    
+    
+    
+    {
+        int qResult_ = 0;
+        sqlite3_stmt* statement_ = NULL;
+        const unsigned char* result_ = NULL;
+        
+        
+        const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'YYYY-ww', '2011-01-02', 'locale should have been here' )\n"
+        "FROM 'Campaigns'\n"
+        "LIMIT 1;";
+        
+        
+        
+        qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+        STAssertTrue( qResult_ == SQLITE_OK, @"unexpected sqlite3_prepare failure" );    
+        
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( qResult_ == SQLITE_ROW, @"query must have only one record - %d", qResult_ );    
+        STAssertTrue( 1 == sqlite3_column_count( statement_ ), @"column count mismatch" );
+        
+        result_ = sqlite3_column_text( statement_, 0 );
+        STAssertTrue( NULL == result_ , @"raw answer mismatch" );
+        
+        
+        qResult_ = sqlite3_step( statement_ );
+        STAssertTrue( SQLITE_DONE == qResult_, @"query must have only one record" );
+        sqlite3_finalize( statement_ );
+    }     
+}
+
+
+-(void)testInvalidFormatLeadsToUndefinedBehaviour
+{
+    int qResult_ = 0;
+    sqlite3_stmt* statement_ = NULL;
+    const unsigned char* result_ = NULL;
+
+
+    const char* query_ = "SELECT ObjcFormatAnsiDateUsingLocale( 'abrakadabra', '2011-01-02', 'en_US' )\n"
+    "FROM 'Campaigns'\n"
+    "LIMIT 1;";
+
+
+    qResult_ = sqlite3_prepare( self->_db, query_, (int)strlen( (char*)query_ ), &statement_, NULL );
+    STAssertTrue( qResult_ == SQLITE_OK, @"unexpected sqlite3_prepare failure" );    
+
+    qResult_ = sqlite3_step( statement_ );
+    STAssertTrue( qResult_ == SQLITE_ROW, @"query must have only one record - %d", qResult_ );    
+    STAssertTrue( 1 == sqlite3_column_count( statement_ ), @"column count mismatch" );
+
+    result_ = sqlite3_column_text( statement_, 0 );
+    STAssertTrue( 0 == strcmp( (const char*)result_, "AM" ) , @"raw answer mismatch" );
+
+
+    qResult_ = sqlite3_step( statement_ );
+    STAssertTrue( SQLITE_DONE == qResult_, @"query must have only one record" );
+    sqlite3_finalize( statement_ );
 }
 
 @end
