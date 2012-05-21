2b10417cb920e96c6a0f8f50c706021d07318455
dodikk <dodikk88.reg@gmail.com>
2012-05-21 14:51:33 +0300

    code style fix

--- ESLocale/SqliteDateFormat/SqliteFunctions.m
+++ ESLocale/SqliteDateFormat/SqliteFunctions.m
@@ -55,7 +55,7 @@ void ObjcFormatAnsiDateUsingLocale( sqlite3_context* ctx_, int argc_, sqlite3_va
         
         NSString* result_ = [ fmt_ getFormattedDate ];
         
-        
+
         if ( nil == result_ || [ result_ isEqualToString: @"" ] )
         {    
             sqlite3_result_null( ctx_ );
