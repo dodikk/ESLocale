b0120fd074c9dc7bf224ae516fe35173b26317db
Vladimir Gorbenko <volodg@mail.ru>
2012-05-22 11:39:31 +0300

    add ceil date with Year component

--- ESLocaleTests/DateAlignmentTest.m
+++ ESLocaleTests/DateAlignmentTest.m
@@ -56,6 +56,42 @@ static NSString* stringFromDate( NSDate* string_ )
     STAssertEqualObjects( result_, @"2012-01-01", @"ok" );
 }
 
-//////////////////// YEAR ROUND ////////////////////
+//////////////////// YEAR CEIL ////////////////////
+
+-(void)testCeilMar31_2011_YearDateResolution
+{
+    NSDate* date_ = dateFromString( @"2011-03-31" );
+
+    NSCalendar* calendar_ = [ ESLocaleFactory gregorianCalendar ];
+    date_ = [ calendar_ ceilDate: date_ forResolution: ESYearDateResolution ];
+
+    NSString* result_ = stringFromDate( date_ );
+
+    STAssertEqualObjects( result_, @"2012-01-01", @"ok" );
+}
+
+-(void)testCeilJan01_2010_YearDateResolution
+{
+    NSDate* date_ = dateFromString( @"2010-01-01" );
+
+    NSCalendar* calendar_ = [ ESLocaleFactory gregorianCalendar ];
+    date_ = [ calendar_ ceilDate: date_ forResolution: ESYearDateResolution ];
+
+    NSString* result_ = stringFromDate( date_ );
+
+    STAssertEqualObjects( result_, @"2011-01-01", @"ok" );
+}
+
+-(void)testCeilDec31_2012_YearDateResolution
+{
+    NSDate* date_ = dateFromString( @"2012-12-31" );
+
+    NSCalendar* calendar_ = [ ESLocaleFactory gregorianCalendar ];
+    date_ = [ calendar_ ceilDate: date_ forResolution: ESYearDateResolution ];
+
+    NSString* result_ = stringFromDate( date_ );
+
+    STAssertEqualObjects( result_, @"2013-01-01", @"ok" );
+}
 
 @end
