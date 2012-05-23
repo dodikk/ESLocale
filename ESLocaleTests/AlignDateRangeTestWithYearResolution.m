f8f14f95e5d466f8c9055884b44eb499103f2088
Vladimir Gorbenko <volodg@mail.ru>
2012-05-23 14:55:07 +0300

    add tests for alignDateRange method

--- ESLocaleTests/AlignDateRangeTestWithYearResolution.m
+++ ESLocaleTests/AlignDateRangeTestWithYearResolution.m
@@ -13,6 +13,8 @@
 
 //STODO test asserts
 
+/////////////////// RESULT: YEAR RESOLUTION ///////////////////
+
 -(void)testAlignDates_Oct20_2008_May15_2011_toTwoYearResolution
 {
     NSDate* startDate_ = dateFromString( @"2008-10-20" );
@@ -76,6 +78,8 @@
     STAssertEqualObjects( endDateStr_  , @"2010-12-31", @"ok" );
 }
 
+/////////////////// RESULT: HALF YEAR RESOLUTION ///////////////////
+
 -(void)testAlignDates_May20_2010_May15_2011_toHalfYearResolution
 {
     NSDate* startDate_ = dateFromString( @"2010-05-20" );
@@ -96,6 +100,8 @@
     STAssertEqualObjects( startDateStr_, @"2010-07-01", @"ok" );
     STAssertEqualObjects( endDateStr_  , @"2010-12-31", @"ok" );
 }
+
+/////////////////// RESULT: QUARTER RESOLUTION ///////////////////
 
 -(void)testAlignDates_Sep20_2011_Mar31_2012_toTwoQuarterResolution
 {
@@ -137,6 +143,59 @@
 
     STAssertEqualObjects( startDateStr_, @"2011-10-01", @"ok" );
     STAssertEqualObjects( endDateStr_  , @"2012-03-31", @"ok" );
+}
+
+/////////////////// RESULT: MONTH RESOLUTION ///////////////////
+
+-(void)testAlignDates_Sep20_2011_Mar31_2012_toFourMonthResolution
+{
+    NSDate* startDate_ = dateFromString( @"2011-10-02" );
+    NSDate* endDate_   = dateFromString( @"2012-03-30" );
+
+    NSCalendar* calendar_ = [ ESLocaleFactory gregorianCalendar ];
+    ESDateResolution resolution_ = ESYearDateResolution;
+
+    [ calendar_ alignDateRangeFromDate: &startDate_
+                                toDate: &endDate_
+                            resolution: &resolution_ ];
+
+    STAssertEquals( ESMonthDateResolution, resolution_, @"ok" );
+
+    NSString* startDateStr_ = stringFromDate( startDate_ );
+    NSString* endDateStr_   = stringFromDate( endDate_ );
+
+    STAssertEqualObjects( startDateStr_, @"2011-11-01", @"ok" );
+    STAssertEqualObjects( endDateStr_  , @"2012-02-29", @"ok" );
+}
+
+/////////////////// RESULT: WEEK RESOLUTION ///////////////////
+
+//May 2012
+//Su Mo Tu We Th Fr Sa
+//      1  2  3  4  5
+//6  7  8  9 10 11 12
+//13 14 15 16 17 18 19
+//20 21 22 23 24 25 26
+//27 28 29 30 31
+-(void)testAlignDates_May02_2012_May24_2012_toTwoWeekResolution
+{
+    NSDate* startDate_ = dateFromString( @"2012-05-02" );
+    NSDate* endDate_   = dateFromString( @"2012-05-24" );
+
+    NSCalendar* calendar_ = [ ESLocaleFactory gregorianCalendar ];
+    ESDateResolution resolution_ = ESYearDateResolution;
+
+    [ calendar_ alignDateRangeFromDate: &startDate_
+                                toDate: &endDate_
+                            resolution: &resolution_ ];
+
+    STAssertEquals( ESWeekDateResolution, resolution_, @"ok" );
+
+    NSString* startDateStr_ = stringFromDate( startDate_ );
+    NSString* endDateStr_   = stringFromDate( endDate_ );
+
+    STAssertEqualObjects( startDateStr_, @"2012-05-06", @"ok" );
+    STAssertEqualObjects( endDateStr_  , @"2012-05-19", @"ok" );
 }
 
 @end
