4ad2540e5a3c9c801c2a83d57c9271117a6da259
dodikk <dodikk88.reg@gmail.com>
2012-05-28 10:19:43 +0300

    vlg - fixed constructor invocations

--- ESLocale/Calendar/Details/NSDateComponents+AddingTimeIntervals.m
+++ ESLocale/Calendar/Details/NSDateComponents+AddingTimeIntervals.m
@@ -6,42 +6,42 @@
 
 +(id)dayComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.day = interval_;
     return result_;
 }
 
 +(id)weekComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.week = interval_;
     return result_;
 }
 
 +(id)monthComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.month = interval_;
     return result_;
 }
 
 +(id)quarterComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.month = ESCalendarMonthPerQuarter * interval_;
     return result_;
 }
 
 +(id)halfYearComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.month = ESCalendarMonthPerHalfYear * interval_;
     return result_;
 }
 
 +(id)yearAlignComponentsWithTimeInterval:( NSInteger )interval_
 {
-    NSDateComponents* result_ = [ NSDateComponents new ];
+    NSDateComponents* result_ = [ self new ];
     result_.year = interval_;
     return result_;
 }
