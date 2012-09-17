09d76d3614d99912d34579af7955b8532b56206b
dodikk <dodikk88.reg@gmail.com>
2012-09-18 00:56:01 +0300

    added code sample

---
+++ article/slides/DateFormatterExample.h
@@ -0,0 +1,13 @@
+//
+//  DateFormatterExample.h
+//  
+//
+//  Created by Oleksandr Dodatko on 18/09/2012.
+//  Copyright (c) 2012 Embedded Sources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateFormatterExample : NSObject
+
+@end
---
+++ article/slides/DateFormatterExample.m
@@ -0,0 +1,13 @@
+//
+//  DateFormatterExample.m
+//  
+//
+//  Created by Oleksandr Dodatko on 18/09/2012.
+//  Copyright (c) 2012 Embedded Sources. All rights reserved.
+//
+
+#import "DateFormatterExample.h"
+
+@implementation DateFormatterExample
+
+@end
