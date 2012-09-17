08d028658d59a2ece1cc24b23a659f106ac608a7
dodikk <dodikk88.reg@gmail.com>
2012-09-18 00:56:32 +0300

    added code sample

--- article/slides/DateFormatterExample.m
+++ article/slides/DateFormatterExample.m
@@ -6,8 +6,18 @@
 //  Copyright (c) 2012 Embedded Sources. All rights reserved.
 //
 
+
 #import "DateFormatterExample.h"
+#import <Foundation/Foundation.h>
 
 @implementation DateFormatterExample
+
+-(NSDate*)parseDate:( NSDate* )date_
+{
+   NSDateFormatter* df_ = [ NSDateFormatter new ];
+   result_.dateFormat = @"yyyy-MM-dd";
+   
+   
+}
 
 @end
