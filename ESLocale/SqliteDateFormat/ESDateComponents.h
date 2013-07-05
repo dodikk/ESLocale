7fd04ad688f5888586b1e75d496ef873febb3dc8
dodikk <dodikk88.reg@gmail.com>
2013-07-05 18:09:21 +0300

    Added C structures for year and quarter processing

---
+++ ESLocale/SqliteDateFormat/ESDateComponents.h
@@ -0,0 +1,19 @@
+#ifndef ESLocale_ESDateComponents_h
+#define ESLocale_ESDateComponents_h
+
+#import <Foundation/Foundation.h>
+
+typedef struct ESYearAndQuarterPOD
+{
+    NSInteger year;
+    NSInteger quarter;
+} ESYearAndQuarterPOD;
+
+
+typedef struct ESYearAndHalfYearPOD
+{
+    NSInteger year;
+    NSInteger halfYear;
+} ESYearAndHalfYearPOD;
+
+#endif
