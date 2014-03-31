98e419b7f5163ad09463e56be026d5533940e80b
dodikk <dodikk88.reg@gmail.com>
2014-03-31 18:45:05 +0300

    [ja] Fixed appledoc comments

--- ESLocale/StringLocalizer/ESHalfYearLocalizer.h
+++ ESLocale/StringLocalizer/ESHalfYearLocalizer.h
@@ -7,12 +7,14 @@
  Only year should be parametrized.
  
  @"%@ 年 上半期"  --> @"2014 年 上半期"
- @"H1 ''%@"      --> @"H1 '14"
+ @"H1 '%@"      --> @"H1 '14"
  
  
  
- Symbols should be escaped.
-```
+ Symbols are not escaped. 
+ They should be escaped manually prior to using in sqlite queries.
+ 
+ ```
  A string constant is formed by enclosing the string in single quotes ('). A single quote within the string can be encoded by putting two single quotes in a row - as in Pascal. C-style escapes using the backslash character are not supported because they are not standard SQL.
 ```
  https://www.sqlite.org/lang_expr.html
@@ -25,11 +27,13 @@
  Only year should be parametrized.
  
   @"%@ 年 下半期"  --> @"2014 年 下半期"
-  @"H2 ''%@"      --> @"H2 '14"
+  @"H2 '%@"      --> @"H2 '14"
  
  
  
- Symbols should be escaped.
+ Symbols are not escaped.
+ They should be escaped manually prior to using in sqlite queries.
+
  ```
  A string constant is formed by enclosing the string in single quotes ('). A single quote within the string can be encoded by putting two single quotes in a row - as in Pascal. C-style escapes using the backslash character are not supported because they are not standard SQL.
  ```
