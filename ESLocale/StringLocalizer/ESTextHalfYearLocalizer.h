8ad2d183bf00b081a46664b599a448ac2a5f7bcd
dodikk <dodikk88.reg@gmail.com>
2014-03-31 19:35:52 +0300

    [ja] exposed a localizer config property

--- ESLocale/StringLocalizer/ESTextHalfYearLocalizer.h
+++ ESLocale/StringLocalizer/ESTextHalfYearLocalizer.h
@@ -3,11 +3,15 @@
 #import <ESLocale/SqliteDateFormat/SqlArgumentParsers/ESYearMode.h>
 #import <Foundation/Foundation.h>
 
+@class ESHalfYearLocalizerPOD;
+
 @interface ESTextHalfYearLocalizer : NSObject<ESHalfYearLocalizer, ESStringLocalizer>
 
 -(instancetype)initWithStringLocalizer:( id<ESStringLocalizer> )localizer
                       firstHalfYearKey:( NSString* )firstHalfKey
                      secondHalfYearKey:( NSString* )secondHalfKey
                               yearMode:( ESYearMode )yearMode;
+
+@property ( nonatomic, readonly ) ESHalfYearLocalizerPOD* halfYearLocalizer;
 
 @end
