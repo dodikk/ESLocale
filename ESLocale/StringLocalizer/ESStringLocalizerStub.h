5a1c2edfe7e3ead5384484cc1790a2253682cc07
dodikk <dodikk88.reg@gmail.com>
2014-04-03 20:41:36 +0300

    [appledoc] Added documentation

--- ESLocale/StringLocalizer/ESStringLocalizerStub.h
+++ ESLocale/StringLocalizer/ESStringLocalizerStub.h
@@ -1,4 +1,22 @@
 #import <ESLocale/StringLocalizer/ESStringLocalizer.h>
 
+/**
+An implementation of ESStringLocalizer protocol that does nothing.
+It is used mostly for unit testing.
+*/
 @interface ESStringLocalizerStub : NSObject< ESStringLocalizer >
+
+
+/**
+This implementation does nothing and returns the key as is.
+
+
+@param key Localization key string.
+
+@return key.
+*/
+-(NSString*)localizedStringForKey:( NSString* )key;
+
 @end
+
+
