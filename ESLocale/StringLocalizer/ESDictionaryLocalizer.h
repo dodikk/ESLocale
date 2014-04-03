da8d21595b341c520647ddd8e2ba1ac75aada79c
dodikk <dodikk88.reg@gmail.com>
2014-04-03 21:00:11 +0300

    [appledoc] Added documentation

--- ESLocale/StringLocalizer/ESDictionaryLocalizer.h
+++ ESLocale/StringLocalizer/ESDictionaryLocalizer.h
@@ -1,7 +1,40 @@
 #import <ESLocale/StringLocalizer/ESStringLocalizer.h>
 
+
+/**
+This class implements the ESStringLocalizer protocol by searching the value in the given dictionary.
+*/
 @interface ESDictionaryLocalizer : NSObject< ESStringLocalizer >
 
+
+/**
+A designated initializer.
+
+@param rawLocalization_ A dictionary for mock localized strings lookup.
+
+@return The localizer object.
+*/
 -(id)initWithDictionary:( NSDictionary* )rawLocalization_;
 
+
+/**
+Initializer not supported. Throws an exception when invoked.
+
+@return nil. It should not return unless assertions are disabled.
+*/
+-(id)init;
+
+
+/**
+Finds the localized value for the given key in the dictionary specified in [ESDictionaryLocalizer initWithDictionary:]
+
+@param key_ A localization key string.
+
+@return rawLocalization_[key_]
+*/
+-(NSString*)localizedStringForKey:( NSString* )key_;
+
 @end
+
+
+
