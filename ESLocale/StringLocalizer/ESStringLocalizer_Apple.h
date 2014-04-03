4fae9c2d25ee65d33deb59d32b8d3546d2757064
dodikk <dodikk88.reg@gmail.com>
2014-04-03 20:46:40 +0300

    [appledoc] Added documentation

--- ESLocale/StringLocalizer/ESStringLocalizer_Apple.h
+++ ESLocale/StringLocalizer/ESStringLocalizer_Apple.h
@@ -1,5 +1,22 @@
 #import <ESLocale/StringLocalizer/ESStringLocalizer.h>
 
+
+/**
+This class implements the ESStringLocalizer protocol bu invoking the NSLocalizedString() macro.
+*/
 @interface ESStringLocalizer_Apple : NSObject< ESStringLocalizer >
 
+
+/**
+A method to return the localized string for a given key.
+
+@param key_ A localization key from the Localizable.strings file.
+
+@return NSLocalizedString( key_, nil )
+*/
+-(NSString*)localizedStringForKey:( NSString* )key_;
+
 @end
+
+
+
