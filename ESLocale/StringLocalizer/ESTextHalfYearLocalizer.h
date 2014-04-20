a62ac14edb24c4fd5d61d78ccc961b60ea2035b6
dodikk <dodikk88.reg@gmail.com>
2014-04-20 12:06:50 +0300

    [appledoc] Added documentation

--- ESLocale/StringLocalizer/ESTextHalfYearLocalizer.h
+++ ESLocale/StringLocalizer/ESTextHalfYearLocalizer.h
@@ -5,13 +5,44 @@
 
 @class ESHalfYearLocalizerPOD;
 
-@interface ESTextHalfYearLocalizer : NSObject<ESHalfYearLocalizer, ESStringLocalizer>
 
+/**
+A half year localizer that gets format strings from a given text localizer.
+Typically, the source is NSLocalizedString() macro.
+*/
+@interface ESTextHalfYearLocalizer : NSObject
+<
+   ESHalfYearLocalizer, 
+   ESStringLocalizer
+>
+
+/**
+Unsupported initializer. Throws an exception.
+
+@return nil if asserts are disabled.
+*/
+-(instancetype)init;
+
+
+/**
+A designated initializer
+
+@param localizer A key-value storage for localized strings.
+@param firstHalfKey a key for the localized first half year format.
+@param secondHalfKey a key for the localized second half year format.
+@param yearMode ESYearModeShort or ESYearModeLong
+
+@return A properly initialized ESTextHalfYearLocalizer object.
+*/
 -(instancetype)initWithStringLocalizer:( id<ESStringLocalizer> )localizer
                       firstHalfYearKey:( NSString* )firstHalfKey
                      secondHalfYearKey:( NSString* )secondHalfKey
                               yearMode:( ESYearMode )yearMode;
 
+/**
+The storage of the half year format strings.
+*/
 @property ( nonatomic, readonly ) ESHalfYearLocalizerPOD* halfYearLocalizer;
 
 @end
+
