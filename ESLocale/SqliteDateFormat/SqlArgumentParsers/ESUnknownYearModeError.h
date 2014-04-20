a0f2064969716cf8a2764e07edf64e4f8aeb302b
dodikk <dodikk88.reg@gmail.com>
2014-04-20 12:20:36 +0300

    [appledoc] Documented an error

--- ESLocale/SqliteDateFormat/SqlArgumentParsers/ESUnknownYearModeError.h
+++ ESLocale/SqliteDateFormat/SqlArgumentParsers/ESUnknownYearModeError.h
@@ -1,9 +1,26 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+An error of parsing the half year mode SQL function argument
+*/
 @interface ESUnknownYearModeError : NSError
 
+/**
+A designated initializer.
+
+@param yearMode A year mode passed to the SQL function.
+
+@return A properly initialized error object.
+*/
 -(instancetype)initWithYearMode:( NSString* )yearMode;
 
+
+/**
+A year mode passed to the SQL function.
+*/
 @property ( nonatomic, readonly ) NSString* actualYearMode;
 
 @end
+
+
