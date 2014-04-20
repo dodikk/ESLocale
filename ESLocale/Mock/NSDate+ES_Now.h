d9584991fe7416a456bf666b70ceab9756bf8904
dodikk <dodikk88.reg@gmail.com>
2014-04-20 14:24:17 +0300

    [appledoc] Documented NSDate mock

--- ESLocale/Mock/NSDate+ES_Now.h
+++ ESLocale/Mock/NSDate+ES_Now.h
@@ -1,7 +1,17 @@
 #import <Foundation/Foundation.h>
 
+
+/**
+A category adding a "Current Date" method that can be safely overriden.
+Warning : do not use [NSDate new] explicitly. Swizzling this method is a bad idea since many unit test frameworks rely on it.
+*/
 @interface NSDate (ES_Now)
 
+/**
+@return The date and time at the moment of calling.
+*/
 +(NSDate*)nowDate;
 
 @end
+
+
