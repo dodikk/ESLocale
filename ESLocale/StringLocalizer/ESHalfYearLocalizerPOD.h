585eed6a081f245735dd1721afe91ffdb47d9ab4
dodikk <dodikk88.reg@gmail.com>
2014-04-20 13:43:59 +0300

    [appledoc] Documented half-year formatter classes

--- ESLocale/StringLocalizer/ESHalfYearLocalizerPOD.h
+++ ESLocale/StringLocalizer/ESHalfYearLocalizerPOD.h
@@ -15,33 +15,77 @@ Unsupported initializer. Throws an exception.
 -(instancetype)init;
 
 
+
 /**
-A designated initializer.
+@param firstHalfFormat  First half year format string. For example, 
+* @"%@ 年 上半期"
+* @"H1 '%@"
 
-@param
-@param
-@param
+@param secondHalfFormat Second half year format string. For example, 
+* @"%@ 年 下半期"
+* @"H2 '%@"
 
-@return
+@param yearTruncationMode An identifier from the SQL function.
+It chould be 'short' or 'long'
+Note : year mode is **case insensitive**
+
+@return A properly initialized ESHalfYearLocalizerPOD object.
 */
 -(instancetype)initWithFirstHalfFormat:( NSString* )firstHalfFormat
                       secondHalfFormat:( NSString* )secondHalfFormat
                     yearTruncationMode:( NSString* )yearTruncationMode;
 
+/**
+  A value passed in the constructor.
+
+  First half year format string. For example, 
+* @"%@ 年 上半期"
+* @"H1 '%@"
+
+*/
 @property ( nonatomic, readonly ) NSString* formatForFirstHalfYear ;
+
+/**
+A value passed in the constructor.
+
+Second half year format string. For example, 
+* @"%@ 年 下半期"
+* @"H2 '%@"
+
+*/
 @property ( nonatomic, readonly ) NSString* formatForSecondHalfYear;
+
+/**
+An identifier from the SQL function.
+It chould be 'short' or 'long'
+Note : year mode is **case insensitive**
+*/
 @property ( nonatomic, readonly ) NSString* yearTruncationMode     ;
+
+
+/**
+A boolean representation of the [ESHalfYearLocalizerPOD yearTruncationMode].
+Throws an exception for the invalid format.
+*/
 @property ( nonatomic, readonly ) BOOL      shouldTruncateYear;
 
 
+/**
+@return A default english formatter
+
+H1 '14
+H2 '14
+*/
 +(instancetype)shortEnglishLocalizer;
+
+/**
+@return A default Japanese formatter.
+
+@"2014 年 下半期"
+@"2014 年 下半期"
+*/
 +(instancetype)longJapaneseLocalizer;
 
 @end
-
-
-
-
-
 
 
